30ee775da55a61a9fb4df3a91e1fc22f13019469
Juan Damian <59286923+damianjuan@users.noreply.github.com>
2022-02-28 17:21:41 -0600

    created gameboard initial layout

--- Candy-Crush-React/src/App.js
+++ Candy-Crush-React/src/App.js
@@ -1,7 +1,29 @@
-function App() {
+import {useState} from 'react';
+
+const width = 8;
+const candyColors = ['blue', 'green', 'orange', 'purple', 'red', 'yellow'];
+
+const App = () => {
+const [currentColorArrangement, setCurrentColorArrangement] = useState([])
+  //board will be 8x8
+  const createBoard = () => {
+    const randomColorArrangement = [];
+    for(let i = 0; i < width*width; i++){
+      const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)]
+    randomColorArrangement.push(randomColor);
+
+    }
+setCurrentColorArrangement(randomColorArrangement)
+  };
+
+
+
+createBoard();
+console.log(currentColorArrangement);
+
   return (
 <div></div>
   );
-}
+};
 
 export default App;
