244c6127095417f5c403f96b04394028854fcdb8
Juan Damian <59286923+damianjuan@users.noreply.github.com>
2022-03-01 11:36:43 -0600

    formating

--- Candy-Crush-React/src/App.js
+++ Candy-Crush-React/src/App.js
@@ -1,36 +1,37 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 const width = 8;
 const candyColors = ['blue', 'green', 'orange', 'purple', 'red', 'yellow'];
 
 const App = () => {
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
 
-  const [currentColorArrangement, setCurrentColorArrangement] = useState([])
- 
   //board will be 8x8 create random colors to fill board
   const createBoard = () => {
     const randomColorArrangement = [];
-    for(let i = 0; i < width*width; i++){
-      const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)]
-    randomColorArrangement.push(randomColor);
-
+    for (let i = 0; i < width * width; i++) {
+      const randomColor =
+        candyColors[Math.floor(Math.random() * candyColors.length)];
+      randomColorArrangement.push(randomColor);
     }
-setCurrentColorArrangement(randomColorArrangement)
+    setCurrentColorArrangement(randomColorArrangement);
   };
 
-// function only gets called once
-useEffect(() => {
-  createBoard();
-}, []);
+  // function only gets called once
+  useEffect(() => {
+    createBoard();
+  }, []);
 
-console.log(currentColorArrangement);
+  console.log(currentColorArrangement);
 
   return (
-<div className='app'>
-<div className='game'>
-
-</div>
-</div>
+    <div className="app">
+      <div className="game">
+        {currentColorArrangement.map((candyColor, index) => (
+          <img key={index} style={{ backgroundColor: candyColor }} />
+        ))}
+      </div>
+    </div>
   );
 };
 
