bcef27ac3c3529cc55f270215dfffec24cfac513
Juan Damian <59286923+damianjuan@users.noreply.github.com>
2022-03-01 11:55:38 -0600

    check for 3 matching column of 3

--- Candy-Crush-React/src/App.js
+++ Candy-Crush-React/src/App.js
@@ -6,6 +6,23 @@ const candyColors = ['blue', 'green', 'orange', 'purple', 'red', 'yellow'];
 const App = () => {
   const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
 
+  const checkForColumnOfThree = () => {
+    for (let i = 0; i < 47; i++) {
+      const columnOfThree = [i, i + width, i + width * 2];
+      const decidedColor = currentColorArrangement[i];
+
+      if (
+        columnOfThree.every(
+          (square) => currentColorArrangement[square] === decidedColor
+        )
+      ) {
+        columnOfThree.forEach(
+          (square) => (currentColorArrangement[square] = '')
+        );
+      }
+    }
+  };
+
   //board will be 8x8 create random colors to fill board
   const createBoard = () => {
     const randomColorArrangement = [];
@@ -21,6 +38,13 @@ const App = () => {
   useEffect(() => {
     createBoard();
   }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfThree();
+    }, 100);
+    return () => clearInterval(timer);
+  }, [checkForColumnOfThree]);
 
   console.log(currentColorArrangement);
 
