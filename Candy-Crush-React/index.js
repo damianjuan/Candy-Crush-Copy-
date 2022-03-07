5b9c27733184d45d3eb88d4512a9539ddb4c0c09
Juan Damian <59286923+damianjuan@users.noreply.github.com>
2022-03-06 23:59:09 -0600

    backend running

---
+++ Candy-Crush-React/index.js
@@ -0,0 +1,6 @@
+const PORT = 8000;
+const axios = require('axios');
+const express = require('express');
+const app = express;
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
